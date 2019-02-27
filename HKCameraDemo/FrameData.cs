87b53599ad172af3c0a5db927c27b8886cef6741
李福荣 <774238577@qq.com>
2019-02-27 08:26:31 +0800

    添加项目文件。

---
+++ HKCameraDemo/FrameData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKCameraDemo
+{
+    /// <summary>
+    /// 帧类型定义
+    /// </summary>
+    public enum VideoType
+    {
+        Unknown = 0,
+        AUDIO16 = 1,
+        RGB32 = 2,
+        YV12 = 3,
+        UYVY = 4,
+        YUV420 = 5,
+        YUY2 = 6,
+        AUDIO8 = 7,
+
+        RGB24 = 9,
+        GRAY = 10,
+
+        RGB555 = 11,
+        RGB565 = 12,
+        YUV422 = 13,
+        YUV444 = 14
+    }
+
+    /// <summary>
+    /// 缓冲视频数据
+    /// </summary>
+    public class FrameData
+    {
+        /// <summary>
+        /// 缓冲取到的视频帧
+        /// </summary>
+        public byte[] FrameBuffer
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 每帧大小
+        /// </summary>
+        public int FrameSize
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 总帧数
+        /// </summary>
+        public int Frames
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 每帧宽度
+        /// </summary>
+        public int FrameWidth
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 每帧高度
+        /// </summary>
+        public int FrameHeight
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 帧类型
+        /// </summary>
+        public VideoType Type
+        {
+            get;
+            set;
+        }
+    }
+
+}
---
+++ HKCameraDemo/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HKCameraDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        D3DImageSource d3dSource;
+
+        private bool fListenStartFlag = false;    //Flags for the server mode having started.
+        private bool fPlaySoundFlag = false;    //Flags for playing sound file
+        public UInt32 fTalkID = 0;
+
+        private int FPlayBackCount = 0;
+        private bool FOutputStartFlag = false;
+
+
+        private bool m_bInitSDK = false;
+        private Int32 m_lUserID = -1;
+        private Int32 m_lRealHandle = -1;
+        private uint iLastErr = 0;
+        private Int32 m_lPort = -1;
+        private IntPtr m_ptrRealHandle = IntPtr.Zero;
+        public CHCNetSDK.NET_DVR_DEVICEINFO_V30 DeviceInfo;
+        private CHCNetSDK.REALDATACALLBACK RealData = null;
+        private PlayCtrl.DECCBFUN m_fDisplayFun = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
